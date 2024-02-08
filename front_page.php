8fbbdb4441266d161c890138d226c2cf0255574b
Abi <159427152+Abinash37@users.noreply.github.com>
2024-02-08 15:15:56 +0530

    add frontpage.php

---
+++ front_page.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Flight Booking</title>
+        <link rel="stylesheet" href="frontstyle.css">
+    </head>
+    <body>
+        <div class="banner">
+            <div class="navbar">
+                <img src="images/logo.png" class="logo">
+                <ul>
+                    <li><a href="search.php">FLIGHT</a></li>
+                    <li><a href="help_bar.php">HELP</a></li>
+                    <li><a href="About_Us.php">ABOUT US</a></li>
+                    <li><a href="contact info.php">CONTACT US</a></li>
+                </ul>
+            </div>
+            <div class="content">
+                <h1>Your Dream Flight</h1>
+                <p>Spend more time in vacation and less time in air</p>
+                <div>
+                    <a href="login.php"><button type="button"><span></span>LOGIN</button>
+                    <a href="Registration table.php"><button type="button"><span></span>REGISTER</button>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
